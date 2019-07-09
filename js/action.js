a3a86b72d385ce0fd9b6b444254428b7e057c7ec
owlPickle <terry998877terry@gmail.com>
2019-07-09 19:44:47 +0800

    menu-big

--- js/action.js
+++ js/action.js
@@ -1,107 +1,107 @@
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
   // slider PlugIn
-  
+
   $('.slick').slick({
-      dots: false,             //顯示輪播圖片會顯示圓圈
-      infinite: true,         //重覆輪播
-      slidesToShow:1,         //輪播顯示個數
-      slidesToScroll: 1,      //輪播捲動個數
-      autoplay: true,         //autoplay : 自動播放
-      responsive: [
- 
+    dots: false,             //顯示輪播圖片會顯示圓圈
+    infinite: true,         //重覆輪播
+    slidesToShow: 1,         //輪播顯示個數
+    slidesToScroll: 1,      //輪播捲動個數
+    autoplay: true,         //autoplay : 自動播放
+    responsive: [
+
       {
         breakpoint: 1000,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows:true
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true
         }
       },
       {
         breakpoint: 320,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows:true
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true
         }
       }]
   });
-  
+
   $('.slick4').slick({
-      dots: false,             //顯示輪播圖片會顯示圓圈
-      infinite: true,         //重覆輪播
-      slidesToShow:4,         //輪播顯示個數
-      slidesToScroll: 4,      //輪播捲動個數
-      autoplay: true,         //autoplay : 自動播放
-      responsive: [
- 
+    dots: false,             //顯示輪播圖片會顯示圓圈
+    infinite: true,         //重覆輪播
+    slidesToShow: 4,         //輪播顯示個數
+    slidesToScroll: 4,      //輪播捲動個數
+    autoplay: true,         //autoplay : 自動播放
+    responsive: [
+
       {
         breakpoint: 900,
         settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            arrows:true
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: true
         }
       },
       {
         breakpoint: 576,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows:false
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
         }
       }]
   });
-  
+
   $('.slick5').slick({
-      dots: false,             //顯示輪播圖片會顯示圓圈
-      infinite: true,         //重覆輪播
-      slidesToShow:5,         //輪播顯示個數
-      slidesToScroll: 2,      //輪播捲動個數
-      autoplay: true,         //autoplay : 自動播放
-      responsive: [
- 
+    dots: false,             //顯示輪播圖片會顯示圓圈
+    infinite: true,         //重覆輪播
+    slidesToShow: 5,         //輪播顯示個數
+    slidesToScroll: 2,      //輪播捲動個數
+    autoplay: true,         //autoplay : 自動播放
+    responsive: [
+
       {
         breakpoint: 1000,
         settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            arrows:false
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false
         }
       },
       {
         breakpoint: 480,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows:false
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
         }
       }]
   });
-  
+
   $('.slick6').slick({
-      dots: false,             //顯示輪播圖片會顯示圓圈
-      infinite: true,         //重覆輪播
-      slidesToShow:6,         //輪播顯示個數
-      slidesToScroll: 4,      //輪播捲動個數
-      autoplay: true,         //autoplay : 自動播放
-      responsive: [
- 
+    dots: false,             //顯示輪播圖片會顯示圓圈
+    infinite: true,         //重覆輪播
+    slidesToShow: 6,         //輪播顯示個數
+    slidesToScroll: 4,      //輪播捲動個數
+    autoplay: true,         //autoplay : 自動播放
+    responsive: [
+
       {
         breakpoint: 1000,
         settings: {
-            slidesToShow: 6,
-            slidesToScroll: 2,
-            arrows:false
+          slidesToShow: 6,
+          slidesToScroll: 2,
+          arrows: false
         }
       },
       {
         breakpoint: 576,
         settings: {
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            arrows:false
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          arrows: false
         }
       }]
   });
@@ -121,101 +121,107 @@ $(document).ready(function(){
     centerMode: true,
     focusOnSelect: true
   });
-  
-  
-  $('#features').on("click", '.right', function(e){
+
+
+  $('#features').on("click", '.right', function (e) {
     $('#features').find('.slick-next').click();
   });
-  
-  $('#features').on("click", '.left', function(e){
+
+  $('#features').on("click", '.left', function (e) {
     $('#features').find('.slick-prev').click();
   });
-  
-  $('#seller').on("click", '.right', function(e){
+
+  $('#seller').on("click", '.right', function (e) {
     $('#seller').find('.slick-next').click();
   });
-  
-  $('#seller').on("click", '.left', function(e){
+
+  $('#seller').on("click", '.left', function (e) {
     $('#seller').find('.slick-prev').click();
   });
-  
-  $('#workspace').on("click", '.right', function(e){
+
+  $('#workspace').on("click", '.right', function (e) {
     $('#workspace').find('.slick-next').click();
   });
-  
-  $('#workspace').on("click", '.left', function(e){
+
+  $('#workspace').on("click", '.left', function (e) {
     $('#workspace').find('.slick-prev').click();
   });
-  
+
   // slider PlugIn end
 
-  // shopping-cart pop
-  $('.shopping-cart').on("click", function(e){
+  // shopping-cart pop, 
+  $('.shopping-cart').on("click", function (e) {
     $('.popout').addClass('pop-open');
   });
 
-  $('.popout').on("click", function(e){
-    
-    var deleteItem = $('.delete'); 
-    
+  $('.popout').on("click", function (e) {
+
+    var deleteItem = $('.delete');
+
     if (e.target != deleteItem) {
-      console.log(deleteItem);
-      console.log(e.target);
-      
-      
+
       $('.popout').removeClass('pop-open');
 
     }
-    
-    
+
+
   });
 
+  // pages pop
+  $('.nav-pages').on("click", function (e) {
+    $('.pages-pop').addClass('open');
+  });
+
+  $('.pages-pop').on("click", function(){
+    $('.pages-pop').removeClass('open');
+  })
+
   // blog-index img-board fade-in 
-  $(document).scroll(function(){
+  $(document).scroll(function () {
     if ($(this).scrollTop() > 900) {
       $('.img-group img').addClass('fade');
     }
   })
   // blog-index scroll to message-wall
-  $('#blog-content .comment').on("click", function(e){
+  $('#blog-content .comment').on("click", function (e) {
     e.preventDefault();
     var offset = $('.message-wall').offset();
-    
-    $('html, body').animate({scrollTop: offset.top});
+
+    $('html, body').animate({ scrollTop: offset.top });
   })
 
   // product-list mobile category dropdown
-  $('.product-sidebar .drop-filter').on("click", function(e){
+  $('.product-sidebar .drop-filter').on("click", function (e) {
     $('.product-sidebar .filter').toggleClass('filter-open');
   });
 
-  $('.product-sidebar .drop-categories').on("click", function(e){
+  $('.product-sidebar .drop-categories').on("click", function (e) {
     $('.product-sidebar .product-categories').toggleClass('categories-open');
   })
 
-  $( function() {
-    $( "#slider-range" ).slider({
+  $(function () {
+    $("#slider-range").slider({
       range: true,
       min: 0,
       max: 600,
-      values: [ 10, 599 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      values: [10, 599],
+      slide: function (event, ui) {
+        $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+      " - $" + $("#slider-range").slider("values", 1));
+  });
 
   // validation 
-  (function() {
+  (function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
       var forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -226,29 +232,32 @@ $(document).ready(function(){
     }, false);
   })();
 
-  $('#procced-checkout').on("click", function(e){
+  $('#procced-checkout').on("click", function (e) {
     $('#validation').click();
   })
 
+  // pages popover 
   
+  $('[data-toggle="popover"]').popover('show');  
+
 
 
 
 
 
   // large slider js practice
-  
+
   // var sliderImg = document.getElementById("slider");
   // var circle1 = document.getElementById("circle1");
   // var circle2 = document.getElementById("circle2");
   // var circle3 = document.getElementById("circle3");
   // var circleIndex = 1;
   // var time = null;
-  
+
   // circle1.addEventListener("click", showImg);
   // circle2.addEventListener("click", showImg);
   // circle3.addEventListener("click", showImg);
-  
+
   // function showImg() {
   //   circleIndex = Number(this.id.substr(6));
   //   sliderImg.innerHTML = '<img src="images/slider/owl'+this.id+'.jpg" alt="counter stool">';
@@ -256,14 +265,14 @@ $(document).ready(function(){
   //   // clearInterval(time);
   //   // timeStart();
   // }
-  
+
   // function circleActive() {
   //   for(var i = 1; i < 4; i++){
   //     document.getElementById("circle" + i).classList.remove("dark");
   //   }
   //   document.getElementById("circle" + circleIndex).classList.add("dark");
   // }
-  
+
   // // function timeStart() {
   // //   time = setInterval(function(){
   // //     circleIndex ++;
@@ -273,16 +282,16 @@ $(document).ready(function(){
   // //     sliderImg.innerHTML = '<img src="images/slider/owlcircle'+circleIndex+'.jpg" alt="counter stool">';
   // //     circleActive();
   // //   }, 1000);
-    
+
   // // }
-  
+
   // // timeStart();
-  
+
   // document.getElementById("circle" + circleIndex).classList.add("dark");
-  
-  
-  
-  
+
+
+
+
 });
 
 
